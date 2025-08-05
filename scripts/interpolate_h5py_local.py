98fd2aefbf8309b0b3e8ce6c20ed02bbda189b4c
David Caldwell <davidjuliancaldwell@gmail.com>
2025-08-04 17:26:28 -0700

    interpolate working for two higher sampling rate cylinder/strip recordings

--- scripts/interpolate_h5py_local.py
+++ scripts/interpolate_h5py_local.py
@@ -1,3 +1,9 @@
+# 
+# due to variability in moberg export and naming, extract strings, flatten them, and compare against subsets 
+# of note, this code assumes that the first two elements in the split string are the relevant ones, proceed at your own peril if not 
+# also , assumes that for waveforms and trends, that each data field comes in pairs, with a timestamp field 2nd. 
+# For eeg/hfeeg, assumes only one timestamp field
+
 # %% imports 
 import h5py
 import numpy as np
@@ -6,6 +12,7 @@ import os
 from pathlib import Path
 import glob
 from py_ecog_utils import interpolate_h5
+import re
 
 #%% get working directory and load in resample rates
 
@@ -24,6 +31,13 @@ files_int = glob.glob(root_path + data_h5 + '/*.h5')
 
 resamp_rates = pd.read_csv(root_path + data +"/H5_resample_rates.csv")
 fs_dict = dict(zip(resamp_rates["Field"].values, resamp_rates["Resample_rate"].values))
+
+fs_dict_split = [re.split(r"[,_]",x.lower()) for x in fs_dict.keys()]
+fs_dict_flat = [x[0] + x[1] for x in fs_dict_split]
+
+# redefine fs_dict to be a flat dictionary
+fs_dict_flatten = dict(zip(fs_dict_flat, fs_dict.values()))
+fs_dict = fs_dict_flatten
 
 #%% run through files and interpolate
 for fn in files_int:
@@ -52,17 +66,61 @@ for fn in files_int:
         #for storing signal gaps
         gaps_dict = dict()
 
-        # EEG
+        resampled_HFEEG = dict()
+        resampled_HFEEG_metadata = dict()
+
         resampled_EEG = dict()
         resampled_EEG_metadata = dict()
+
+        resampled_trends = dict()
+        resampled_trends_metadata = dict()
+
+        resampled_waveforms = dict()
+        resampled_waveforms_metadata = dict()
+
+# check if HFEEG is present, 
+        if any("timestamps" in s.lower() for s in f["EEG"].keys()) and any("hfeeg" in s.lower() for s in f["EEG"].keys()):
+
+            if "EEG" in f.keys():
+                #check if timestamps are present
+                # if timestamps are not present, skip this file
+                    # find the index of the timestamps
+                    index_timestamps_hfeeg = [index_timestamps_hfeeg for index_timestamps_hfeeg, chan in enumerate(f["EEG"].keys()) if "timestamps" in chan.lower()]
+                    keys = [key for key in f["EEG"].keys()]
+
+
+                    for chan in f["EEG"].keys():
+                        if "timestamps" in chan.lower():
+                            continue
+                        if len(f["EEG"][chan][:]) == 0:
+                            continue
+                        resamp_signal_info, gaps = interpolate_h5.process_signal(f["EEG"][chan][:], f["EEG"][keys[index_timestamps_hfeeg[0]]][:], 2000, flip_signal=False, max_gap = (1/2000)*1e6+500000)
+                        if len(resamp_signal_info[0]) == 0 or len(resamp_signal_info[1]) == 0:
+                            continue
+                        resampled_HFEEG[chan] = resamp_signal_info
+                        gaps_dict[chan] = gaps
+                        resampled_HFEEG_metadata[chan] = {"units":f["EEG"][chan].attrs['units'],
+                                        "start_time": min(resampled_HFEEG[chan][0]),
+                                        "end_time": max(resampled_HFEEG[chan][0]),
+                                        "number_points": len(resampled_HFEEG[chan][0]),
+                                        "hz":2000}
+
+        # EEG
+
         if "EEG" in f.keys():
-            if "EEG,Composite,SampleSeries,Composite,Amp1020,timestamps" in f["EEG"].keys():
+            #check if timestamps are present
+            # if timestamps are not present, skip this file
+            if any("timestamps" in s.lower() for s in f["EEG"].keys()):
+                # find the index of the timestamps
+                index_timestamps = [index_timestamps for index_timestamps, chan in enumerate(f["EEG"].keys()) if "timestamps" in chan.lower()]
+                keys = [key for key in f["EEG"].keys()]
+
                 for chan in f["EEG"].keys():
-                    if "Timestamps" in chan:
+                    if 'timestamps' in chan.lower():
                         continue
                     if len(f["EEG"][chan][:]) == 0:
                         continue
-                    resamp_signal_info, gaps = interpolate_h5.process_signal(f["EEG"][chan][:], f["EEG"]["EEG,Composite,SampleSeries,Composite,Amp1020,timestamps"][:], 256, flip_signal=False, max_gap = (1/256)*1e6+500000)
+                    resamp_signal_info, gaps = interpolate_h5.process_signal(f["EEG"][chan][:], f["EEG"][keys[index_timestamps[0]]][:], 256, flip_signal=False, max_gap = (1/256)*1e6+500000)
                     if len(resamp_signal_info[0]) == 0 or len(resamp_signal_info[1]) == 0:
                         continue
                     resampled_EEG[chan] = resamp_signal_info
@@ -73,75 +131,75 @@ for fn in files_int:
                                             "number_points": len(resampled_EEG[chan][0]),
                                             "hz":256}
 
-        resampled_HFEEG = dict()
-        resampled_HFEEG_metadata = dict()
-        if "EEG" in f.keys():
-            if "HFEEG_Composite_Timestamps" in f["EEG"].keys():
-                for chan in f["EEG"].keys():
-                    if "Timestamps" in chan:
-                        continue
-                    if len(f["EEG"][chan][:]) == 0:
-                        continue
-                    resamp_signal_info, gaps = interpolate_h5.process_signal(f["EEG"][chan][:], f["EEG"]["HFEEG_Composite_Timestamps"][:], 2000, flip_signal=False, max_gap = (1/2000)*1e6+500000)
-                    if len(resamp_signal_info[0]) == 0 or len(resamp_signal_info[1]) == 0:
-                        continue
-                    resampled_HFEEG[chan] = resamp_signal_info
-                    gaps_dict[chan] = gaps
-                    resampled_HFEEG_metadata[chan] = {"units":f["EEG"][chan].attrs['units'],
-                                    "start_time": min(resampled_HFEEG[chan][0]),
-                                    "end_time": max(resampled_HFEEG[chan][0]),
-                                    "number_points": len(resampled_HFEEG[chan][0]),
-                                    "hz":2000}
-
         # Trends
-        resampled_trends = dict()
-        resampled_trends_metadata = dict()
+
         if "Trends" in f.keys():
             if len(f["Trends"].keys()) >= 1:
-                for signal_key in f["Trends"]:
-                    if "Timestamps" in signal_key:
-                        continue
-                    if len(f["Trends"][signal_key][:]) == 0:
-                        continue
-                    if signal_key not in fs_dict.keys():
-                        print(f"{signal_key} sample rate uknown. skipping. add field with fs to csv table and rerun.")
-                        continue
-                    resamp_signal_info, gaps = interpolate_h5.process_signal(f["Trends"][signal_key][:], f["Trends"][signal_key + "_Timestamps"][:], fs_dict[signal_key], 
-                                                                flip_signal=False, max_gap = (1/fs_dict[signal_key])*1e6+500000)
-                    if len(resamp_signal_info[0]) == 0 or len(resamp_signal_info[1]) == 0:
-                        continue
-                    resampled_trends[signal_key] = resamp_signal_info
-                    gaps_dict[signal_key] = gaps
-                    resampled_trends_metadata[signal_key] = {"units":f["Trends"][signal_key].attrs['units'],
-                                    "start_time": min(resampled_trends[signal_key][0]),
-                                    "end_time": max(resampled_trends[signal_key][0]),
-                                    "number_points": len(resampled_trends[signal_key][0]),
-                                    "hz":fs_dict[signal_key]}
+                if any("timestamps" in s.lower() for s in f["Trends"].keys()):
+                    keys = [key for key in f["Trends"].keys()]
+                    
+                    for signal_key in f["Trends"]:
+                        index_int = keys.index(signal_key)
+
+                        #make flatten list of hdf5 keys to match against
+                        signal_key_split_flatten_temp = re.split(r"[,_]",signal_key.lower())
+                        signal_key_split_flatten = signal_key_split_flatten_temp[0] + signal_key_split_flatten_temp[1]
+
+                        if "timestamps" in signal_key.lower():
+                            continue
+                        if len(f["Trends"][signal_key][:]) == 0:
+                            continue
+                        if signal_key_split_flatten not in fs_dict.keys():
+                            print(f"{signal_key} sample rate uknown. skipping. add field with fs to csv table and rerun.")
+                            continue
+                        resamp_signal_info, gaps = interpolate_h5.process_signal(f["Trends"][signal_key][:], f["Trends"][keys[index_int+1]][:], fs_dict[signal_key_split_flatten], 
+                                                                    flip_signal=False, max_gap = (1/fs_dict[signal_key_split_flatten])*1e6+500000)
+                        if len(resamp_signal_info[0]) == 0 or len(resamp_signal_info[1]) == 0:
+                            continue
+                        resampled_trends[signal_key] = resamp_signal_info
+                        gaps_dict[signal_key] = gaps
+                        resampled_trends_metadata[signal_key] = {"units":f["Trends"][signal_key].attrs['units'],
+                                        "start_time": min(resampled_trends[signal_key][0]),
+                                        "end_time": max(resampled_trends[signal_key][0]),
+                                        "number_points": len(resampled_trends[signal_key][0]),
+                                        "hz":fs_dict[signal_key_split_flatten]}
 
         # Waveforms
-        resampled_waveforms = dict()
-        resampled_waveforms_metadata = dict()
+
+
         if "Waveforms" in f.keys():
             if len(f["Waveforms"]) >= 1:
-                for signal_key in f["Waveforms"]:        
-                    if "Timestamps" in signal_key:
-                        continue
-                    if len(f["Waveforms"][signal_key][:]) == 0:
-                        continue
-                    if signal_key not in fs_dict.keys():
-                        print(f"{signal_key} sample rate uknown. skipping. add field with fs to csv table and rerun.")
-                        continue
-                    resamp_signal_info, gaps = interpolate_h5.process_signal(f["Waveforms"][signal_key][:], f["Waveforms"][signal_key + "_Timestamps"][:], fs_dict[signal_key], 
-                                                                flip_signal=False, max_gap = (1/fs_dict[signal_key])*1e6+500000)
-                    if len(resamp_signal_info[0]) == 0 or len(resamp_signal_info[1]) == 0:
-                        continue
-                    resampled_waveforms[signal_key] = resamp_signal_info
-                    gaps_dict[signal_key] = gaps 
-                    resampled_waveforms_metadata[signal_key] = {"units":f["Waveforms"][signal_key].attrs['units'],
-                            "start_time": min(resampled_waveforms[signal_key][0]),
-                            "end_time": max(resampled_waveforms[signal_key][0]),
-                            "number_points": len(resampled_waveforms[signal_key][0]),
-                            "hz":fs_dict[signal_key]}
+                if any("timestamps" in s.lower() for s in f["Waveforms"].keys()):
+                    keys = [key for key in f["Waveforms"].keys()]
+
+                    for signal_key in keys:
+                        index_int = keys.index(signal_key)
+
+                        #make flatten list of hdf5 keys to match against
+                        signal_key_split_flatten_temp = re.split(r"[,_]",signal_key.lower())
+                        signal_key_split_flatten = signal_key_split_flatten_temp[0] + signal_key_split_flatten_temp[1]
+
+                        if "timestamps" in signal_key.lower():
+                            continue
+                        if len(f["Waveforms"][signal_key][:]) == 0:
+                            continue
+                        if signal_key_split_flatten not in fs_dict.keys():
+                            print(f"{signal_key} sample rate unknown. skipping. add field with fs to csv table and rerun.")
+                            continue
+                        resamp_signal_info, gaps = interpolate_h5.process_signal(f["Waveforms"][signal_key][:], f["Waveforms"][keys[index_int + 1]][:], fs_dict[signal_key_split_flatten], 
+                                                                    flip_signal=False, max_gap = (1/fs_dict[signal_key_split_flatten])*1e6+500000)
+                        if len(resamp_signal_info[0]) == 0 or len(resamp_signal_info[1]) == 0:
+                            continue             
+                        resampled_waveforms[signal_key] = resamp_signal_info
+                        gaps_dict[signal_key] = gaps 
+                        resampled_waveforms_metadata[signal_key] = {"units":f["Waveforms"][signal_key].attrs['units'],
+                                "start_time": min(resampled_waveforms[signal_key][0]),
+                                "end_time": max(resampled_waveforms[signal_key][0]),
+                                "number_points": len(resampled_waveforms[signal_key][0]),
+                                "hz":fs_dict[signal_key_split_flatten]}
+
+
+        
 
 
         # save upsampled results to a new file
